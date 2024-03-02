e5f75353dd1959fd792d1959f78faeb3d9bef81e
te <winerte@163.com>
2024-03-02 19:05:48 +0800

    springboot + mysql + druid

---
+++ springboot-mysql/src/main/java/com/winerte/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.winerte.controller;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
+import com.winerte.pojo.User;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private DataSource dataSource;
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/get/{id}")
+    public User getUserById(@PathVariable("id")String id) throws Exception {
+        System.out.println(id);
+        Connection connection = dataSource.getConnection();
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println(druidDataSource.getMaxActive());
+        String sql="select * from user where id= ?";
+       User user = jdbcTemplate.queryForObject(sql,new BeanPropertyRowMapper<>(User.class),id);
+        System.out.println(user);
+        druidDataSource.close();
+        connection.close();
+        return null;
+    }
+
+    @GetMapping("/test")
+    private String test(){
+        return "test";
+    }
+}
---
+++ springboot-mysql/src/main/java/com/winerte/pojo/User.java
@@ -0,0 +1,14 @@
+package com.winerte.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private long id;
+
+    private String userName;
+
+    private int sex;
+
+    private String addr;
+}
